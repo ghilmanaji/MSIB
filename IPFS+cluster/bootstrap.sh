1d84209394f48e6862e83b1aed8f4bbe4f618e1e
Ghilman Aji <139083228+ghilmanaji@users.noreply.github.com>
2024-04-05 09:48:28 +0700

    Add files via upload

---
+++ IPFS+cluster/bootstrap.sh
@@ -0,0 +1,4 @@
+export LIBP2P_FORCE_PNET=1
+ipfs bootstrap rm --all
+ipfs bootstrap add /ip4/45.77.38.105/tcp/4001/ipfs/12D3KooWPhNbzobvBJJgPLnDqAvx5vgAPzaqGw9DZnMSE9cp3qeD
+ipfs bootstrap add /ip4/139.180.133.85/tcp/4001/ipfs/12D3KooWDrQygcaENF2JXj2Wbsrm9k1y54QaYhhZrqmiVrDbpkiW
